5445739f634ccf6294e469a3b4cd7760e3cf0587
Cyril <hourdebaigt_cyril@orange.fr>
2026-01-28 10:49:49 +0100

    Récupération des projets avec DOctrine

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EmployeFactory;
+use App\Factory\ProjetFactory;
+use App\Factory\TacheFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+final class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // 1) Employés
+        EmployeFactory::createMany(10);
+
+        // 2) Projets
+        $projets = ProjetFactory::createMany(5);
+
+        // 3) Tâches : 10 tâches par projet, avec statuts variés
+        foreach ($projets as $projet) {
+            TacheFactory::createMany(4, [
+                'projet' => $projet,
+                'statut' => 'To Do',
+                'employe' => null
+            ]);
+            TacheFactory::createMany(3, [
+                'projet' => $projet,
+                'statut' => 'Doing',
+                'employe' => null
+            ]);
+            TacheFactory::createMany(3, [
+                'projet' => $projet,
+                'statut' => 'Done',
+                'employe' => null
+            ]);
+        }
 
         $manager->flush();
     }
