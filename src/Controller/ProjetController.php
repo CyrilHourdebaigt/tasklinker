eb1314bdb03a5018c714374c75bd0b021497333c
Cyril <hourdebaigt_cyril@orange.fr>
2026-02-03 11:17:48 +0100

    Modifications Projet

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Projet;
+use App\Form\ProjetType;
 use App\Repository\ProjetRepository;
 use App\Repository\TacheRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -33,5 +36,59 @@ final class ProjetController extends AbstractController
             'tachesDoing' => $tachesDoing,
             'tachesDone' => $tachesDone,
         ]);
+    }
+
+    #[Route('/{id}/edit', name: 'projet_edit', methods: ['GET', 'POST'], requirements: ['id' => '\d+'])]
+    public function edit(Projet $projet, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ProjetType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('projet_show', [
+                'id' => $projet->getId(),
+            ]);
+        }
+
+        return $this->render('projet/edit.html.twig', [
+            'projet' => $projet,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route('/projets/nouveau', name: 'projet_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $projet = new Projet();
+
+        $form = $this->createForm(ProjetType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($projet);
+            $em->flush();
+
+            return $this->redirectToRoute('projet_show', [
+                'id' => $projet->getId(),
+            ]);
+        }
+
+        return $this->render('projet/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/projets/{id}/archive', name: 'projet_archive', methods: ['POST'])]
+    public function archive(Projet $projet, Request $request, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('archive_projet_' . $projet->getId(), $request->request->get('_token'))) {
+            $projet->setArchive(true);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_home');
     }
 }
